48600d0ad89da5f844c1d7843ec301f32eed2387
damien <damiencs00@gmail.com>
2024-03-04 21:45:25 -0500

    Linked firebase

--- dist/app.js
+++ dist/app.js
@@ -6,8 +6,26 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.get("/", (req, res) => {
-    res.send("Hello World!");
+const admin = require("firebase-admin");
+const serviceAccount = require("../../student-app-86f45-firebase-adminsdk-k6kov-e3bb5e1890");
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://student-app-86f45-default-rtdb.firebaseio.com",
+});
+const db = admin.database();
+module.exports = db;
+// Example of reading data from Firebase Realtime Database
+app.get("/read", (req, res) => {
+    db.ref("user")
+        .once("value")
+        .then((snapshot) => {
+        const data = snapshot.val();
+        res.send(data);
+    })
+        .catch((error) => {
+        console.log("Error reading data from Firebase:", error);
+        res.status(500).send("Error reading data from Firebase.");
+    });
 });
 app.listen(port, () => {
     console.log(`Express is listening at http://localhost:${port}`);
--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,29 @@
 import express, { Application, Request, Response } from "express";
 const app: Application = express();
 const port: number = 3000;
+const admin = require("firebase-admin");
+const serviceAccount = require("../../student-app-86f45-firebase-adminsdk-k6kov-e3bb5e1890");
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World!");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://student-app-86f45-default-rtdb.firebaseio.com",
+});
+
+const db = admin.database();
+module.exports = db;
+
+// Example of reading data from Firebase Realtime Database
+app.get("/read", (req: Request, res: Response) => {
+  db.ref("user")
+    .once("value")
+    .then((snapshot: any) => {
+      const data = snapshot.val();
+      res.send(data);
+    })
+    .catch((error: any) => {
+      console.log("Error reading data from Firebase:", error);
+      res.status(500).send("Error reading data from Firebase.");
+    });
 });
 
 app.listen(port, () => {
